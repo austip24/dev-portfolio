92286b68c3e171f48253ff1d3083705323826e18
austip24 <austinpier24@gmail.com>
2022-07-18 06:54:14 -0700

    added LinkItem for next.js Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+
+interface LinkItemProps {
+	href: string;
+	path: string;
+	children: React.ReactNode;
+}
+
+const LinkItem: React.FC<LinkItemProps> = ({ href, path, children }) => {
+	return (
+		<Link href={href}>
+			<a>{children}</a>
+		</Link>
+	);
+};
 
 interface NavbarProps {
-  path: string
+	path: string;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ path }) => {
-  return (
-    <nav><Link href='/works'>Works</Link></nav>
-  )
-}
+	return (
+		<nav>
+			<LinkItem href="/works" path={path}>
+				Works
+			</LinkItem>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
