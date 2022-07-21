68c491450a67e5b9f59871bc88bf4b8373b9ac5d
austip24 <austinpier24@gmail.com>
2022-07-21 11:04:17 -0700

    added date to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
 	return (
 		<footer className="text-center mb-6 dark:text-slate-400 text-sm fixed bottom-0 left-0 w-screen">
-			&copy; Austin Pierson. All Rights Reserved.
+			&copy; 2022 Austin Pierson. All Rights Reserved.
 		</footer>
 	);
 };
