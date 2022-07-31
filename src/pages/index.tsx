be55aca60ef50799beb926283313ed15e1acb5f6
austip24 <austinpier24@gmail.com>
2022-07-31 02:02:36 -0700

    added new profile_pic.png

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,8 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
 	return (
-		<div className="p-8 gap-6 md:gap-10 flex flex-col-reverse items-center justify-center md:flex-row">
-			<motion.div
-				className="tracking-tight md:flex md:flex-col md:gap-1 text-2xl sm:text-4xl md:text-5xl font-bold"
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				transition={{ duration: 1 }}
-			>
+		<div className="p-8 gap gap-4 md:gap-10 flex flex-col-reverse items-center justify-center md:flex-row">
+			<div className="tracking-tight md:flex md:flex-col md:gap-1 text-2xl md:text-4xl lg:text-5xl font-bold">
 				<h1 className="inline md:block">Hi, </h1>
 				<h1 className="inline md:block">
 					I&apos;m{" "}
@@ -20,15 +15,16 @@ const Home: NextPage = () => {
 					,{" "}
 				</h1>
 				<h1 className="inline md:block">a web developer</h1>
-			</motion.div>
-			<motion.div className="relative w-52 sm:w-64 md:w-72 aspect-square rounded-full overflow-hidden outline outline-offset-4 outline-slate-900 dark:outline-current pointer-events-none">
+			</div>
+			<div className="relative w-52 sm:w-64 md:w-72 aspect-square rounded-full overflow-hidden outline outline-offset-4 outline-current backdrop-blur-sm">
 				<Image
+					className="md:translate-y-1"
 					src="/imgs/profile_pic.png"
 					alt="Profile Pic"
 					layout="fill"
 					objectFit="cover"
 				/>
-			</motion.div>
+			</div>
 		</div>
 	);
 };
