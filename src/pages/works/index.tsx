2e1877d4794a69f6e7312371bc203aca72d374e7
austip24 <austinpier24@gmail.com>
2022-08-22 04:18:31 -0700

    created initial structure of WorkCard

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -2,14 +2,43 @@ import React from "react";
 import type { NextPage } from "next";
 import Link from "next/link";
 import works from "../../data/works";
+import { Work } from "../../types";
+import Image from "next/image";
+import Section from "../../components/layouts/Section";
+
+const WorkCard: React.FC<Work> = (props) => {
+	return (
+		<div className="group relative cursor-pointer border-2 border-slate-500 rounded-lg">
+			<Link href={`/works/${props.title}`}>
+				<div className="flex flex-col">
+					<div className="relative w-full aspect-video">
+						<div className="absolute inset-0 group-hover:bg-black/5 dark:group-hover:bg-white/5 z-10"></div>
+						<Image
+							src="/imgs/profile.png"
+							layout="fill"
+							objectFit="cover"
+							alt={`Thumbnail for ${props.title}`}
+						/>
+					</div>
+					<div className="group-hover:bg-black/5 dark:group-hover:bg-white/5 group-hover:text-black dark:group-hover:text-white">
+						<div className="p-4 flex flex-col gap-1 md:gap-4 group-hover:-translate-y-1 transition-transform duration-150">
+							<h2 className="font-extrabold">{props.title}</h2>
+							<p>{props.description}</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		</div>
+	);
+};
 
 const Works: NextPage = () => {
 	return (
-		<div className="grid grid-cols-2">
+		<div className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-4 py-4">
 			{works.map((work, i) => (
-				<Link href={`/works/${work.title}`} key={i}>
-					{work.title}
-				</Link>
+				<>
+					<WorkCard key={i} {...work} />
+				</>
 			))}
 		</div>
 	);
